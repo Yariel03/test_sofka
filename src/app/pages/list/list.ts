ded36e055b4ceb9f82b884c157aed6783b15ade0
Yariel <alex-003.net@live.com>
2025-07-16 22:43:56 -0500

    (fix): correccion de busqueda e implementacion de effect

--- src/app/pages/list/list.ts
+++ src/app/pages/list/list.ts
@@ -1,4 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 import { ICreditCard } from '../../interfaces/IProducts.interface';
 import { SwBancoService } from '../../services/swBanco.service';
 import { CIcon } from '../../shared/c-icon/c-icon';
@@ -10,46 +17,53 @@ import { FormsModule } from '@angular/forms';
   imports: [CIcon, CAvatar, FormsModule],
   templateUrl: './list.html',
   styleUrl: './list.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class List {
   swBancoService = inject(SwBancoService);
+
   lstProductos = signal([] as ICreditCard[]);
   cpyProducts = signal([] as ICreditCard[]);
+  dataFilter = signal<string>('');
+  viewData = signal<number>(5);
 
-  dataFilter: string = '';
-  viewData: number = 5;
+  total = computed(() => {
+    return this.cpyProducts().length;
+  });
+
+  loadingEffect = effect(() => {
+    this.cpyProducts.set(this.lstProductos().slice(0, this.viewData()));
+  });
 
   ngOnInit() {
     this.swBancoService.getProductos().subscribe({
       next: (res) => {
         this.lstProductos.set(res.data);
-        this.cpyProducts.set(res.data);
-        console.log(this.lstProductos());
       },
     });
   }
 
   search() {
-    if (this.dataFilter.length == 0) {
-      this.cpyProducts.set(this.lstProductos());
+    if (this.dataFilter().length == 0) {
+      this.cpyProducts.set(this.lstProductos().slice(0, this.viewData()));
+      return;
     }
-    // setTimeout(() => {
-    console.log('search' + this.dataFilter);
-    const products = this.cpyProducts().filter(
-      (product) =>
-        product.name.toLowerCase().includes(this.dataFilter.toLowerCase()) ||
-        product.description
-          .toLowerCase()
-          .includes(this.dataFilter.toLowerCase()) ||
-        product.id.toString().includes(this.dataFilter) ||
-        product.date_revision.toString().includes(this.dataFilter) ||
-        product.date_release.toString().includes(this.dataFilter)
-    );
-    this.cpyProducts.set(products);
-    // }, 500);
-  }
+    setTimeout(() => {
+      this.cpyProducts.set(this.lstProductos());
 
-  selectView() {
-    this.lstProductos.set(this.cpyProducts().slice(0, this.viewData));
+      const products = this.cpyProducts().filter(
+        (product) =>
+          product.name
+            .toLowerCase()
+            .includes(this.dataFilter().toLowerCase()) ||
+          product.description
+            .toLowerCase()
+            .includes(this.dataFilter().toLowerCase()) ||
+          product.id.toString().includes(this.dataFilter()) ||
+          product.date_revision.toString().includes(this.dataFilter()) ||
+          product.date_release.toString().includes(this.dataFilter())
+      );
+      this.cpyProducts.set(products.slice(0, this.viewData()));
+    }, 800);
   }
 }
